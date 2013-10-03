1cb6969ea45b5d6bed4003290f9e8fb4cc116d5b
xplodwild <xplod@ouverta.fr>
2013-10-03 22:46:34 +0200

    Settings: Add bridge to PerformanceControl
    Change-Id: I259323366ca0aac8b8d334bc9629dec5acc9108a
    
    Conflicts:
    
    	Android.mk
    	res/values/custom_strings.xml
    	res/xml/settings_headers.xml
    	src/com/android/settings/Settings.java
    
    Conflicts:
    	Android.mk
    	res/xml/settings_headers.xml

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1172,4 +1172,5 @@ public class Settings extends PreferenceActivity
     public static class QuickSettingsTilesSettingsActivity extends Settings { /* empty */ }
     public static class BatteryIconStyleSettingsActivity extends Settings { /* empty */ }
     public static class DisplayRotationSettingsActivity extends Settings { /* empty */ }
+    public static class PerformanceSettingsActivity extends Settings { /* empty */ }
 }
