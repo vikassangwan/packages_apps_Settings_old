dskim9752/android_packages_apps_Settings

Quick Settings: Weather tile
You can check the weather to make it easier.

Dokdo Project - Gwon Hyeok

src/com/android/settings/quicksettings/QuickSettingsUtil.java
@@ -45,6 +45,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_SYNC;
 import static com.android.internal.util.cm.QSConstants.TILE_TORCH;
 import static com.android.internal.util.cm.QSConstants.TILE_USER;
 import static com.android.internal.util.cm.QSConstants.TILE_VOLUME;
+import static com.android.internal.util.cm.QSConstants.TILE_WEATHER;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFI;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFIAP;
 
@@ -161,6 +162,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_INTERNALMEMORY, R.string.internal_memory_tile,
                 "com.android.systemui:drawable/ic_qs_internalmemory"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_WEATHER, R.string.tile_weather,
+                "com.android.systemui:drawable/weather_preferences"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {

vikassangwan/packages_apps_Settings_old

Conflicts:
	res/values-ko/dokdo_strings.xml
	res/values/dokdo_strings.xml
	src/com/android/settings/quicksettings/QuickSettingsUtil.java

src/com/android/settings/ose/quicksettings/QuickSettingsUtil.java
@@ -55,6 +55,7 @@ import static com.android.internal.util.ose.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.ose.QSConstants.TILE_WIFI;
 import static com.android.internal.util.ose.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.ose.QSConstants.TILE_REBOOT;
+import static com.android.internal.util.ose.QSConstants.TILE_WEATHER;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -170,6 +171,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_CONTACT, R.string.title_tile_contact,
                 "com.android.systemui:drawable/ic_qs_default_user"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_WEATHER, R.string.tile_weather,
+                "com.android.systemui:drawable/weather_preferences"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {