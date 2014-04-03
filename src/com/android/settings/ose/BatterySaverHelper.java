PAC-ROM/android_packages_apps_Settings

[3/3] Settings: Battery Saver mode Vanir-ized by me:P
PS2: rebased
PS3: Ditched brightness change since it`s useless and added data
on/off
PS4: WI-FI on/off
PS5: derp fixes

introduce battery saver mode
this based from what user want or what user do to saving the battery life
this include:
1. Automatically switch mobile network into what user choice and also disable mobile data IF wifi connected to an AP
2. Automatically switch mobile network and disable mobile data IF battery low (battery level can configure)
3. Reduce screen brightness based low battery (this feature will disabled if Auto brightness mode enabled)
4. Not changing network IF on Call or Usb tether enabled or WifiAP enabled
5. Not changing network IF there is Download/upload progress
6. Support For Both GSM and CDMA devices (need someone to test for CDMA devices)
7. every condition has an option :D
8. Restore all network configuration if Smart Radio disabled
9. Add Time Range to start Smart Radio

Change-Id: I19502c731b74e82a3873f19f51965ee5a88c1f04
Signed-off-by: Dan Trifan <jimsth@gmail.com>

src/com/android/settings/vanir/BatterySaverHelper.java
@@ -1,0 +1,137 @@
+/*
+ * Copyright (C) 2014 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.provider.Settings;
+
+import java.util.Calendar;
+
+public class BatterySaverHelper {
+
+    private final static String TAG = "BatterySaverHelper";
+
+    private static final String SCHEDULE_BATTERY_SAVER =
+            "com.android.settings.vanir.SCHEDULE_BATTERY_SAVER";
+
+    private static final int FULL_DAY = 1440; // 1440 minutes in a day
+
+    // Pending intent to start/stop service
+    private static PendingIntent makeServiceIntent(Context context,
+            String action, int requestCode) {
+        Intent intent = new Intent(context, BatterySaverReceiver.class);
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(
+                context, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+    }
+
+    public static boolean deviceSupportsMobileData(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE);
+    }
+
+    public static void scheduleService(Context context) {
+        boolean batterySaverEnabled = Settings.Global.getInt(context.getContentResolver(),
+                Settings.Global.BATTERY_SAVER_OPTION, 0) != 0;
+        int batterySaverStart = Settings.Global.getInt(context.getContentResolver(),
+                Settings.Global.BATTERY_SAVER_START, 0);
+        int batterySaverEnd = Settings.Global.getInt(context.getContentResolver(),
+                Settings.Global.BATTERY_SAVER_END, 0);
+        Intent serviceTriggerIntent = (new Intent())
+                   .setClassName("com.android.systemui", "com.android.systemui.batterysaver.BatterySaverService");
+        PendingIntent startIntent = makeServiceIntent(context, SCHEDULE_BATTERY_SAVER, 1);
+        PendingIntent stopIntent = makeServiceIntent(context, SCHEDULE_BATTERY_SAVER, 2);
+        AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        am.cancel(startIntent);
+        am.cancel(stopIntent);
+
+        if (!batterySaverEnabled) {
+            context.stopService(serviceTriggerIntent);
+            return;
+        }
+
+        if (batterySaverStart == batterySaverEnd) {
+            // 24 hours, start without stop
+            context.startService(serviceTriggerIntent);
+            return;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        int currentMinutes = calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(Calendar.MINUTE);
+
+        boolean inBatterySaver = false;
+        // time from now on (in minutes) when the service start/stop should be scheduled
+        int serviceStartMinutes = -1, serviceStopMinutes = -1;
+
+        if (batterySaverEnd < batterySaverStart) {
+            // Starts at night, ends in the morning.
+            if (currentMinutes >= batterySaverStart) {
+                inBatterySaver = true;
+                serviceStopMinutes = FULL_DAY - currentMinutes + batterySaverEnd;
+            } else if (currentMinutes <= batterySaverEnd) {
+                inBatterySaver = true;
+                serviceStopMinutes = batterySaverEnd - currentMinutes;
+            } else {
+                inBatterySaver = false;
+                serviceStartMinutes = batterySaverStart - currentMinutes;
+                serviceStopMinutes = FULL_DAY - currentMinutes + batterySaverEnd;
+            }
+        } else {
+            // Starts in the morning, ends at night.
+            if (currentMinutes >= batterySaverStart && currentMinutes <= batterySaverEnd) {
+                inBatterySaver = true;
+                serviceStopMinutes = batterySaverEnd - currentMinutes;
+            } else {
+                inBatterySaver = false;
+                if (currentMinutes <= batterySaverStart) {
+                    serviceStartMinutes = batterySaverStart - currentMinutes;
+                    serviceStopMinutes = batterySaverEnd - currentMinutes;
+                } else {
+                    serviceStartMinutes = FULL_DAY - currentMinutes + batterySaverStart;
+                    serviceStopMinutes = FULL_DAY - currentMinutes + batterySaverEnd;
+                }
+            }
+        }
+
+        if (inBatterySaver) {
+            context.startService(serviceTriggerIntent);
+        } else {
+            context.stopService(serviceTriggerIntent);
+        }
+
+        if (serviceStartMinutes >= 0) {
+            // Start service a minute early
+            serviceStartMinutes--;
+            calendar.add(Calendar.MINUTE, serviceStartMinutes);
+            am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), startIntent);
+            calendar.add(Calendar.MINUTE, -serviceStartMinutes);
+        }
+
+        if (serviceStopMinutes >= 0) {
+            // Stop service a minute late
+            serviceStopMinutes++;
+            calendar.add(Calendar.MINUTE, serviceStopMinutes);
+            am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), stopIntent);
+            calendar.add(Calendar.MINUTE, -serviceStopMinutes);
+        }
+    }
+}

src/com/android/settings/vanir/BatterySaverReceiver.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2014 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BatterySaverReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (BatterySaverHelper.deviceSupportsMobileData(context)) {
+            BatterySaverHelper.scheduleService(context);
+        }
+    }
+
+}

vikassangwan/packages_apps_Settings_old

Conflicts:
	AndroidManifest.xml
	res/values/ose_arrays.xml
	res/values/pac_attrs.xml
	res/values/pac_strings.xml
	res/xml/system_ui_settings.xml
	src/com/android/settings/cyanogenmod/BootReceiver.java
	src/com/android/settings/cyanogenmod/SystemUiSettings.java

src/com/android/settings/ose/BatterySaverHelper.java
@@ -1,0 +1,137 @@
+/*
+ * Copyright (C) 2014 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.ose;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.provider.Settings;
+
+import java.util.Calendar;
+
+public class BatterySaverHelper {
+
+    private final static String TAG = "BatterySaverHelper";
+
+    private static final String SCHEDULE_BATTERY_SAVER =
+            "com.android.settings.ose.SCHEDULE_BATTERY_SAVER";
+
+    private static final int FULL_DAY = 1440; // 1440 minutes in a day
+
+    // Pending intent to start/stop service
+    private static PendingIntent makeServiceIntent(Context context,
+            String action, int requestCode) {
+        Intent intent = new Intent(context, BatterySaverReceiver.class);
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(
+                context, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+    }
+
+    public static boolean deviceSupportsMobileData(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE);
+    }
+
+    public static void scheduleService(Context context) {
+        boolean batterySaverEnabled = Settings.Global.getInt(context.getContentResolver(),
+                Settings.Global.BATTERY_SAVER_OPTION, 0) != 0;
+        int batterySaverStart = Settings.Global.getInt(context.getContentResolver(),
+                Settings.Global.BATTERY_SAVER_START, 0);
+        int batterySaverEnd = Settings.Global.getInt(context.getContentResolver(),
+                Settings.Global.BATTERY_SAVER_END, 0);
+        Intent serviceTriggerIntent = (new Intent())
+                   .setClassName("com.android.systemui", "com.android.systemui.batterysaver.BatterySaverService");
+        PendingIntent startIntent = makeServiceIntent(context, SCHEDULE_BATTERY_SAVER, 1);
+        PendingIntent stopIntent = makeServiceIntent(context, SCHEDULE_BATTERY_SAVER, 2);
+        AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        am.cancel(startIntent);
+        am.cancel(stopIntent);
+
+        if (!batterySaverEnabled) {
+            context.stopService(serviceTriggerIntent);
+            return;
+        }
+
+        if (batterySaverStart == batterySaverEnd) {
+            // 24 hours, start without stop
+            context.startService(serviceTriggerIntent);
+            return;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        int currentMinutes = calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(Calendar.MINUTE);
+
+        boolean inBatterySaver = false;
+        // time from now on (in minutes) when the service start/stop should be scheduled
+        int serviceStartMinutes = -1, serviceStopMinutes = -1;
+
+        if (batterySaverEnd < batterySaverStart) {
+            // Starts at night, ends in the morning.
+            if (currentMinutes >= batterySaverStart) {
+                inBatterySaver = true;
+                serviceStopMinutes = FULL_DAY - currentMinutes + batterySaverEnd;
+            } else if (currentMinutes <= batterySaverEnd) {
+                inBatterySaver = true;
+                serviceStopMinutes = batterySaverEnd - currentMinutes;
+            } else {
+                inBatterySaver = false;
+                serviceStartMinutes = batterySaverStart - currentMinutes;
+                serviceStopMinutes = FULL_DAY - currentMinutes + batterySaverEnd;
+            }
+        } else {
+            // Starts in the morning, ends at night.
+            if (currentMinutes >= batterySaverStart && currentMinutes <= batterySaverEnd) {
+                inBatterySaver = true;
+                serviceStopMinutes = batterySaverEnd - currentMinutes;
+            } else {
+                inBatterySaver = false;
+                if (currentMinutes <= batterySaverStart) {
+                    serviceStartMinutes = batterySaverStart - currentMinutes;
+                    serviceStopMinutes = batterySaverEnd - currentMinutes;
+                } else {
+                    serviceStartMinutes = FULL_DAY - currentMinutes + batterySaverStart;
+                    serviceStopMinutes = FULL_DAY - currentMinutes + batterySaverEnd;
+                }
+            }
+        }
+
+        if (inBatterySaver) {
+            context.startService(serviceTriggerIntent);
+        } else {
+            context.stopService(serviceTriggerIntent);
+        }
+
+        if (serviceStartMinutes >= 0) {
+            // Start service a minute early
+            serviceStartMinutes--;
+            calendar.add(Calendar.MINUTE, serviceStartMinutes);
+            am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), startIntent);
+            calendar.add(Calendar.MINUTE, -serviceStartMinutes);
+        }
+
+        if (serviceStopMinutes >= 0) {
+            // Stop service a minute late
+            serviceStopMinutes++;
+            calendar.add(Calendar.MINUTE, serviceStopMinutes);
+            am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), stopIntent);
+            calendar.add(Calendar.MINUTE, -serviceStopMinutes);
+        }
+    }
+}

src/com/android/settings/ose/BatterySaverReceiver.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2014 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.ose;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BatterySaverReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (BatterySaverHelper.deviceSupportsMobileData(context)) {
+            BatterySaverHelper.scheduleService(context);
+        }
+    }
+
+}
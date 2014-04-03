d7314dbb664a10b6c6c262e8d51b4854d51e2289
Squadzone <ai.the.smarties.physics@gmail.com>
2014-04-02 21:56:28 -0400

    [3/3] Settings: Battery Saver mode Vanir-ized by me:P
    PS2: rebased
    PS3: Ditched brightness change since it`s useless and added data
    on/off
    PS4: WI-FI on/off
    PS5: derp fixes
    
    introduce battery saver mode
    this based from what user want or what user do to saving the battery life
    this include:
    1. Automatically switch mobile network into what user choice and also disable mobile data IF wifi connected to an AP
    2. Automatically switch mobile network and disable mobile data IF battery low (battery level can configure)
    3. Reduce screen brightness based low battery (this feature will disabled if Auto brightness mode enabled)
    4. Not changing network IF on Call or Usb tether enabled or WifiAP enabled
    5. Not changing network IF there is Download/upload progress
    6. Support For Both GSM and CDMA devices (need someone to test for CDMA devices)
    7. every condition has an option :D
    8. Restore all network configuration if Smart Radio disabled
    9. Add Time Range to start Smart Radio
    
    Change-Id: I19502c731b74e82a3873f19f51965ee5a88c1f04
    Signed-off-by: Dan Trifan <jimsth@gmail.com>
    
    Conflicts:
    	AndroidManifest.xml
    	res/values/ose_arrays.xml
    	res/values/pac_attrs.xml
    	res/values/pac_strings.xml
    	res/xml/system_ui_settings.xml
    	src/com/android/settings/cyanogenmod/BootReceiver.java
    	src/com/android/settings/cyanogenmod/SystemUiSettings.java

---
+++ src/com/android/settings/ose/BootReceiver.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.ose;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.android.settings.DisplaySettings;
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.hardware.DisplayColor;
+import com.android.settings.hardware.DisplayGamma;
+import com.android.settings.hardware.VibratorIntensity;
+import com.android.settings.location.LocationSettings;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "BootReceiver";
+    private static final String KSM_SETTINGS_PROP = "sys.ksm.restored";
+
+    @Override
+    public void onReceive(Context ctx, Intent intent) {
+
+        if (Utils.fileExists(MemoryManagement.KSM_RUN_FILE)) {
+            if (SystemProperties.getBoolean(KSM_SETTINGS_PROP, false) == false
+                    && intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+                SystemProperties.set(KSM_SETTINGS_PROP, "true");
+                configureKSM(ctx);
+            } else {
+                SystemProperties.set(KSM_SETTINGS_PROP, "false");
+            }
+        }
+
+        if (BatterySaverHelper.deviceSupportsMobileData(ctx)) {
+            BatterySaverHelper.scheduleService(ctx);
+        }
+
+        /* Restore the hardware tunable values */
+        DisplayColor.restore(ctx);
+        DisplayGamma.restore(ctx);
+        VibratorIntensity.restore(ctx);
+        DisplaySettings.restore(ctx);
+        LocationSettings.restore(ctx);
+
+        Intent service = new Intent(ctx, BootService.class);
+        ctx.startService(service);
+    }
+
+    private void configureKSM(Context ctx) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+
+        boolean ksmDefault = (SystemProperties.get("ro.ksm.default", "0") != "0");
+        boolean ksm = prefs.getBoolean(MemoryManagement.KSM_PREF, ksmDefault);
+
+        Utils.fileWriteOneLine(MemoryManagement.KSM_RUN_FILE, ksm ? "1" : "0");
+        Log.d(TAG, "KSM settings restored.");
+    }
+}
