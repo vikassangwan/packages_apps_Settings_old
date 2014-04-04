d071f02a53243796be688e0991ab7f89ecc64422
timpohladthomas <timpohladthomas@gmail.com>
2014-04-03 22:01:06 -0400

    Fixes

--- src/com/android/settings/ose/BootReceiver.java
+++ src/com/android/settings/ose/BootReceiver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The ose Project
+ * Copyright (C) 2012 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,13 +24,8 @@ import android.os.SystemProperties;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
-import com.android.settings.DisplaySettings;
 import com.android.settings.R;
 import com.android.settings.Utils;
-import com.android.settings.hardware.DisplayColor;
-import com.android.settings.hardware.DisplayGamma;
-import com.android.settings.hardware.VibratorIntensity;
-import com.android.settings.location.LocationSettings;
 import com.android.settings.ose.BatterySaverHelper;
 
 import java.util.Arrays;
@@ -39,41 +34,14 @@ import java.util.List;
 public class BootReceiver extends BroadcastReceiver {
 
     private static final String TAG = "BootReceiver";
-    private static final String KSM_SETTINGS_PROP = "sys.ksm.restored";
 
     @Override
     public void onReceive(Context ctx, Intent intent) {
 
-        if (Utils.fileExists(MemoryManagement.KSM_RUN_FILE)) {
-            if (SystemProperties.getBoolean(KSM_SETTINGS_PROP, false) == false
-                    && intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-                SystemProperties.set(KSM_SETTINGS_PROP, "true");
-                configureKSM(ctx);
-            } else {
-                SystemProperties.set(KSM_SETTINGS_PROP, "false");
-            }
-        }
-
             BatterySaverHelper.scheduleService(ctx);
-
-        /* Restore the hardware tunable values */
-        DisplayColor.restore(ctx);
-        DisplayGamma.restore(ctx);
-        VibratorIntensity.restore(ctx);
-        DisplaySettings.restore(ctx);
-        LocationSettings.restore(ctx);
 
         Intent service = new Intent(ctx, BootService.class);
         ctx.startService(service);
     }
 
-    private void configureKSM(Context ctx) {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
-
-        boolean ksmDefault = (SystemProperties.get("ro.ksm.default", "0") != "0");
-        boolean ksm = prefs.getBoolean(MemoryManagement.KSM_PREF, ksmDefault);
-
-        Utils.fileWriteOneLine(MemoryManagement.KSM_RUN_FILE, ksm ? "1" : "0");
-        Log.d(TAG, "KSM settings restored.");
-    }
 }
